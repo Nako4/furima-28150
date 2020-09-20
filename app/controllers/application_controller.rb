b9fcf7397ff02ccad158545c9a77f767fca39ece
Nako4 <papnoatelier@gmail.com>
2020-09-20 15:08:27 +0900

    テーブルへデータの保存

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
   
-  # protected
-
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age])
-  # end
-
   private
 
   def basic_auth
@@ -14,4 +9,9 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_kanji, :last_kanji, :first_katakana, :last_katakana, :birthday])
+  end
+
 end
