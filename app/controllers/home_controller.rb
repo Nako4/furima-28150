c38f0db4701268b2cccfa7746cad6d75ba6d6e8e
Nako4 <papnoatelier@gmail.com>
2020-09-27 08:37:17 +0900

    rubocopで自動修正+自分で修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,4 @@ class HomeController < ApplicationController
 
   def new
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, length: { maximum: 40 }
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   validates :first_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
   validates :last_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
-  validates :first_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角文字を使用してください' } 
-  validates :last_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角文字を使用してください' } 
+  validates :first_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角文字を使用してください' }
+  validates :last_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角文字を使用してください' }
   validates :birthday, presence: true
   validates :email, uniqueness: true
 
-  #メールアドレスに@必須の記述はconfig/initialize/devise.rbにデフォルトで記述がある
-  #パスワードは6-128文字の制限がconfig/initialize/devise.rbにデフォルトで記述がある
-  
+  # メールアドレスに@必須の記述はconfig/initialize/devise.rbにデフォルトで記述がある
+  # パスワードは6-128文字の制限がconfig/initialize/devise.rbにデフォルトで記述がある
 end
