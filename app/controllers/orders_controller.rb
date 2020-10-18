c65a952ee42611c8727a9d20d489e6dade64f04e
Nako4 <papnoatelier@gmail.com>
2020-10-18 13:09:30 +0900

    購入処理の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,10 +2,10 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @user_order = UserOrder.new
   end
 
   def new
-    @user_order = UserOrder.new
   end
 
   def create
@@ -16,14 +16,15 @@ class OrdersController < ApplicationController
       @user_order.save
       redirect_to root_path
     else
-      render :create
+      @item = Item.find(params[:item_id])
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.permit(:postal_code, :consignor_id, :city, :addresses, :building, :phone_number, :user_id, :item_id, :token).merge(user_id: current_user.id, price: @price)
+    params.require(:user_order).permit(:postal_code, :consignor_id, :city, :addresses, :building, :phone_number, :user_id, :item_id, :token).merge(user_id: current_user.id, price: @price)
   end
 
   def pay_item
