64b86976d4a6aa5926a2b58392ee83b53977fcc4
Nako4 <papnoatelier@gmail.com>
2020-10-23 20:24:44 +0900

    ログインしてないユーザーの処理を変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :redirect_root
   before_action :item_set, only: [:index, :create]
-  before_action :authenticate_user!, only: [:index]
 
   def index
     @user_order = UserOrder.new
